30b22af39be41dd5535ced75bf68b2f4e1682a50
Dheeraj Mahra <dheerajmahra873@gmail.com>
2021-09-04 20:33:22 +0530

    configured separate configs for dev and prod envs

--- webpack.common.js
+++ webpack.common.js
@@ -2,14 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: {
+    app: "./src/js/index.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  devServer: {
-    port: 9000,
-    open: true,
+    filename: "[name].bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
